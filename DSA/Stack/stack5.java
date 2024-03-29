34f42c913cffc1b0a194c9c5095f5991c0264f56
dikshagour01 <gourdiksha9880@gmail.com>
2024-03-29 09:15:05 +0530

    stack5 question updated

--- DSA/Stack/stack5.java
+++ DSA/Stack/stack5.java
@@ -2,30 +2,27 @@
 
 import java.util.*;
 public class stack5{
-    public static void reverseString(String str){
-        Stack<Character> s = new Stack<>();
-        int idx=0;
-
-        //push character into stack
-        while(idx<str.length()){
-            s.push(str.charAt(idx));
-            idx++;
-        }
-
-        //pop character from stack
-        StringBuilder result = new StringBuilder("");
-        while(!s.isEmpty()){
-            char curr = s.pop();
-            result.append(curr);
-        }
-
-        str=result.toString();
-        System.out.println(str);
-
+   public static String reverseString(String str){
+    Stack<Character> s = new Stack<>();
+    int idx=0;
+    //push the element in the stack
+    while(idx<str.length()){
+        s.push(str.charAt(idx));
+        idx++;
     }
+   
+    //pop the elements from the stack
+    StringBuilder result = new StringBuilder("");
+    while(!s.isEmpty()){
+        char curr = s.pop();
+        result.append(curr);
+    }
+
+    return result.toString();
+   }
     public static void main(String args[]){
         String str="abcd";
         System.out.println(str);
-        reverseString(str);
+        System.out.println(reverseString(str));
     }
 }
