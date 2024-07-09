b63b6b4fef2e3ce3a0f134da16a9cc903d07806a
dikshagour01 <gourdiksha9880@gmail.com>
2024-07-09 11:14:34 +0530

    Count node question add

---
+++ DSA/Binary_tress/Count_node.java
@@ -0,0 +1,37 @@
+public class Count_node{
+     static class Node {  // created a node class
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static int CountNode(Node root){
+        if(root==null){
+            return 0;
+        }
+
+        int leftCount = CountNode(root.left);
+        int rightCount = CountNode(root.right);
+        return leftCount+rightCount+1;
+    }
+
+    public static void main(String args[]){
+
+
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        System.out.println(CountNode(root));
+    }
+}
