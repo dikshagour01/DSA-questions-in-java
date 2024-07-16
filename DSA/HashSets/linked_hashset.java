3937c5850b2f41413e41c0dabf72d8be2f63ab9e
dikshagour01 <gourdiksha9880@gmail.com>
2024-07-16 10:45:18 +0530

    tree set and linked hash set added

---
+++ DSA/HashSets/linked_hashset.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class linked_hashset{
+    public static void main(String args[]){
+        LinkedHashSet<String> lhs = new LinkedHashSet<>();
+        lhs.add("Delhi");
+        lhs.add("Jaipur");
+        lhs.add("Bangaluru");
+        lhs.add("Pune");
+        
+        System.out.println(lhs);
+
+        
+    }
+}
---
+++ DSA/HashSets/treeset.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class treeset{
+    public static void main(String args[]){
+        TreeSet<String> ts = new TreeSet<>();
+
+        ts.add("Delhi");
+        ts.add("Jaipur");
+        ts.add("Bangaluru");
+        ts.add("Pune");
+        
+        System.out.println(ts);
+    }
+}
